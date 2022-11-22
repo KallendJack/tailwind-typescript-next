9b9f62138142ba8f48946d2449d3001b8911fab0
Jack Kallend <Kallendo96@live.co.uk>
2022-11-22 18:18:06 +0000

    chore(setup): remove unnecessary content / libraries

--- src/stores/header/useNavigationStore.js
+++
@@ -1,7 +0,0 @@
-import create from 'zustand'
-
-export const useNavigationStore = create((set) => ({
-    navOpen: false,
-    toggle: () => set((state) => ({ navOpen: !state.navOpen })),
-    close: () => set({ navOpen: false }),
-}))
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,30 +17,11 @@ module.exports = {
             primary: ['Montserrat', 'sans-serif'],
         },
         extend: {
-            colors: {
-                primary: '#001126',
-                primaryGradientEnd: '#00204F',
-                secondary: '#F2652C',
-                secondaryGradientEnd: '#FF4900',
-                tertiary: '#03BFB5',
-                quaternary: '#7F8FA7',
-                quaternaryGradientEnd: '#A4A9BE',
-                grey: '#C9CBD8',
-                cardGradientStart: '#00204f',
-                cardGradientEnd: '#004869',
-            },
+            colors: {},
             padding: {
                 sm: '4rem',
                 md: '6rem',
                 lg: '10rem',
-            },
-            boxShadow: {
-                default: '0 6px 12px 0 rgba(0,17,38,0.12)',
-                defaultHover: '0 12px 24px 0 rgba(0,0,0,0.12)',
-                button: '0 12px 24px 0 rgba(0,0,0,0.12)',
-            },
-            borderRadius: {
-                default: '0.625rem',
             },
         },
     },
